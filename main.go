7d375b69407621f9af9f9ca76e199df66cd2b67b
den <denisha.madhura2007@gmail.com>
2026-05-11 18:31:39 +0530

    added proper functioning
    cursor and ansi issues yet to be fixed

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"io"
 	"os"
 	"os/exec"
-	"time"
+	"regexp"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 	"github.com/creack/pty"
 )
+
+var ansiRegex = regexp.MustCompile(`\x1b\[[0-9;]*[a-zA-Z]|\x1b\](?:[^\x07\x1b]*)(?:\x07|\x1b\\)|[\x00-\x08\x0b\x0c\x0e-\x1f]`)
+
+func stripAnsi(s string) string {
+	return ansiRegex.ReplaceAllString(s, "")
+}
 
 func main() {
 	a := app.New()
@@ -19,7 +26,7 @@ func main() {
 	ui := widget.NewTextGrid()
 	ui.SetText("You are now using goterm")
 
-	c := exec.Command("/bin/zsh")
+	c := exec.Command("/bin/bash")
 	p, err := pty.Start(c)
 	if err != nil {
 		fyne.LogError("Failed to open pty", err)
@@ -28,20 +35,47 @@ func main() {
 
 	defer c.Process.Kill()
 
-	p.Write([]byte("ls\r"))
-	time.Sleep(1 * time.Second)
-	b := make([]byte, 1024)
-	_, err = p.Read(b)
-	if err != nil {
-		fyne.LogError("Failed to read pty", err)
-	}
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := p.Read(buf)
+			if err != nil {
+				if err != io.EOF {
+					fyne.LogError("PTY read error", err)
+				}
+				return
+			}
+			clean := stripAnsi(string(buf[:n]))
+			fyne.Do(func() {
+				ui.SetText(ui.Text() + clean)
+			})
+		}
+	}()
 
-	w.SetContent(
-		fyne.NewContainerWithLayout(
-			layout.NewGridWrapLayout(fyne.NewSize(420, 200)),
-			ui,
-		),
-	)
-
+	w.Canvas().SetOnTypedRune(func(r rune) {
+		p.Write([]byte(string(r)))
+	})
+	w.Canvas().SetOnTypedKey(func(e *fyne.KeyEvent) {
+		switch e.Name {
+		case fyne.KeyEnter, fyne.KeyReturn:
+			p.Write([]byte("\r"))
+		case fyne.KeyBackspace:
+			p.Write([]byte{127})
+		case fyne.KeyUp:
+			p.Write([]byte("\x1b[A"))
+		case fyne.KeyDown:
+			p.Write([]byte("\x1b[B"))
+		case fyne.KeyLeft:
+			p.Write([]byte("\x1b[D"))
+		case fyne.KeyRight:
+			p.Write([]byte("\x1b[C"))
+		case fyne.KeyTab:
+			p.Write([]byte("\t"))
+		case fyne.KeyEscape:
+			p.Write([]byte("\x1b"))
+		}
+	})
+	w.SetContent(container.NewMax(ui))
+	w.Resize(fyne.NewSize(800, 500))
 	w.ShowAndRun()
 }
